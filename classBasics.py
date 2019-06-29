5e543615f40c033e06b9b8a6aa80a667d9a68368
Jonathan Reed <johnny_s_reed@yahoo.com>
2019-06-29 12:33:33 -0500

    Add files via upload

---
+++ classBasics.py
@@ -0,0 +1,38 @@
+import stdio
+import math
+
+# Author: Jonathan Reed
+# Basic classes + inheritance in python
+
+#base class shape:
+class Shape:
+    def __init__(self, objType):
+        self.objType = objType
+    def printType(self):
+        print(self.objType)
+
+#class Rect inherits from Shape - I.e. Rect "is a" shape
+class Rect(Shape): 
+    def __init__(self, height, width, objType):
+        Shape.__init__(self, objType)
+        self.height = height
+        self.width = width
+    def calcArea(self):
+        area = self.width * self.height
+        return area
+    
+#Class circle inherits from shape as well
+class Circle(Shape):
+     def __init__(self, radius, objType):
+        Shape.__init__(self, objType)
+        self.radius = radius
+     def calcArea(self):
+         area = radius * radius * math.pi
+         return area
+
+#instantiate a Rect object
+square = Rect(2, 2, "Rect")
+#function from child class
+area = square.calcArea()
+#function from parent class, accessed through Rect object (which is a shape!)
+square.printType()
